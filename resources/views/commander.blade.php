4deb835259b7b5fc54e8ffedcff5f9ccae7dabe1
NGAPOUCHE LUDGER <104681689+Ludgerlu@users.noreply.github.com>
2024-07-15 13:56:43 +0100

    update veiw commande

--- resources/views/commander.blade.php
+++ resources/views/commander.blade.php
@@ -21,6 +21,26 @@
 <link rel="stylesheet" href="{{asset('assets/css/responsive.css')}}">
 <link rel="stylesheet" href="{{asset('assets/css/commande.css')}}">
 
+<style>
+    .number-input {
+      display: flex;
+      align-items: center;
+    }
+
+    .number-input button {
+      background-color: #f2f2f2;
+      border: none;
+      padding: 8px 12px;
+      cursor: pointer;
+    }
+
+    .number-input input {
+      width: 80px;
+      padding: 8px;
+      text-align: center;
+      border: 1px solid #ccc;
+    }
+  </style>
 
 </head>
 <body>
@@ -39,8 +59,8 @@
 						</div>
 					</div>
 				</div>
-				<div class="row mb-5">
-                    <div class="best-product best-indicator">
+				<div class="mb-5">
+                    <div class="best-product best-indicator row">
 						<!-- Start single product -->
                         <div class="col-md-6 col-sm-12 col-xs-12 mb-5">
 							<div class="row">
@@ -143,45 +163,51 @@
 
         <div class="modal" id="modal">
             <div class="modal-container">
-                <form id="form" class="form-horizontal row" action="/commande/saveHaut" method="post">
+                <form id="form" action="/commande/saveHaut" method="post">
                     @csrf
-                    <input type="hidden" name="product_name" id="product_name">
-                    
-
+                    <input type="hidden" name="product_name" id="product_name">   
                     <h2>Confimation de livrasion</h2>
-                    <div class="form-group col-xs-12 col-sm-6 col-md-6">
-                        <label for="total">Unique Amount (*FCFA)</label>
-                        <div class="input-group">
-                            <div class="input-group-addon">CFA</div>
-                            <input type="text" class="form-control" name="total" id="total" readonly="readonly">
-                            <div class="input-group-addon">.00</div>
+                    <div class="form-row">
+                        <div class="col-sm-6 my-1">
+                            <label for="total">Unique Amount (*FCFA)</label>
+                            <div class="input-group">
+                                <div class="input-group-prepend">
+                                    <div class="input-group-text">CFA</div>
+                                </div>
+                                <input type="text" class="form-control" name="total" id="total" readonly="readonly">
+                                <div class="input-group-prepend">
+                                    <div class="input-group-text">.00</div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-sm-6 my-1">
+                            <label for="price">Total (*FCFA)</label>
+                            <div class="input-group">
+                                <div class="input-group-prepend">
+                                    <div class="input-group-text">CFA</div>
+                                </div>
+                                <input type="text" class="form-control" name="price" id="price" readonly="readonly">
+                                <div class="input-group-prepend">
+                                    <div class="input-group-text">.00</div>
+                                </div>
+                            </div>
                         </div>
                     </div>
-                    <div class="form-group col-xs-12 col-sm-6 col-md-6">
-                        <label for="price">Total (*FCFA)</label>
-                        <div class="input-group">
-                            <div class="input-group-addon">CFA</div>
-                            <input type="text" class="form-control" name="price" id="price" readonly="readonly">
-                            <div class="input-group-addon">.00</div>
+                    <div class="form-row">
+                        <div class="col-sm-6 my-1">
+                            <label for="number"> Nombre d'article</label><br>
+                            <div class="number-input">
+                                <button onclick="updateAmount(this);">-</button>
+                                <input type="number" class="form-control nombre" name="number" id="number" min="0" value="1"></input>  
+                                <button onclick="updateAmount(this);">+</button>
+                            </div>  
+                        </div>
+                        <div class="col-sm-6 my-1">
+                            <label for="number"> Votre Numero</label><br>
+                            <input type="text" class="form-control" name="telephone" id="telephone" placeholder="+237 6** *** ***"></input>   
                         </div>
                     </div>
                     
-                    <div class="form-group col-md-6 col-sm-12 col-xs-12 m-5">
-
-                        <label for="number"> Nombre d'article</label><br>
-                        <input type="number" class="form-control nombre" name="number" id="number" min="0" value="1"></input>  
-                        
-                        <button type="button" value="plus" class="btn btn-success" onclick="updateAmount(this);">
-                            +  
-                        </button> 
-                        <button type="button" value="minus"  class="btn btn-warning"  onclick="updateAmount(this);">
-                            -  
-                        </button> 
-                    </div>
-                    <div class="form-group col-md-6 col-sm-12 col-xs-12 m-5">
-                        <label for="number"> Votre Numero</label><br>
-                        <input type="text" class="form-control" name="telephone" id="telephone" placeholder="+237 6** *** ***"></input>   
-                    </div>
                     <div class="col-md-12 col-sm-12 col-xs-12">
 						<div class="section-headline text-center">
 						    <button type="submit" class="btn btn-primary">CONFIRM</button>
@@ -203,6 +229,6 @@
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.6/dist/umd/popper.min.js" integrity="sha384-wHAiFfRlMFy6i5SRaxvfOCifBUQy1xHdJ/yoi7FRNXMRBu5WHdZYu1hA6ZOblgut" crossorigin="anonymous"></script>
 
-    
+
 </body>
 </html>
